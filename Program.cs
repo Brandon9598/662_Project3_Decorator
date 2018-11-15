2befc2ea03c06a494d8f82b75c62af0747ebbd65
Brandon Hancock <Brandon9598@gmail.com>
2018-11-14 21:22:47 -0500

    adding all the code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _657_Project3_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Collect TV type
+            Console.WriteLine("What types of TV are you looking for?");
+
+            // Collect Resolution color
+            Console.WriteLine("What screen resolution are you looking for");
+
+            // Smart TV
+
+            // 3D, Curved, or none
+        }
+    }
+}
